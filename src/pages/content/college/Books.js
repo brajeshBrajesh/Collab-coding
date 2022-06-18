c0f99ca0b62f1165a8b12d49f09f67b3a904edfd
brajeshBrajesh <singh.brajesh8189250@gmail.com>
2022-06-18 18:04:47 +0530

    overlays added
    overlays added to college section :->Books,Notes and Qn papers

--- src/pages/content/college/Books.js
+++ src/pages/content/college/Books.js
@@ -2,9 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 import { getDatabase, ref, child, get, push, set } from "firebase/database";
 import { useSelector } from "react-redux";
 import AddButton from "../AddButton";
- 
+
 import BooksDetail from "../../../components/BooksDetail";
 import BooksAddDet from "../../../components/BooksAddDet";
+import styles from "./Books.module.css";
+
 import {
   getStorage,
   ref as sref,
@@ -16,62 +18,59 @@ export default function Books() {
   const [toDisplaData, setToDisplayData] = useState([]);
   const [bookFilters, setBookFilters] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [progress,setProgress] = useState(0);
-  const [showform,setShowform] = useState();
-    const subject = useRef();
-    const author = useRef();
-    const BookName = useRef();
-    const sem = useRef();
+  const [progress, setProgress] = useState(0);
+  const [showform, setShowform] = useState();
+  const subject = useRef();
+  const author = useRef();
+  const BookName = useRef();
+  const sem = useRef();
   const dbRef = ref(getDatabase());
   const db = getDatabase();
-      
-     const bookRef = ref(db,'content/college/Books');
-     const bookUID = push(bookRef); 
-      
-     //
-    function uploadHandler(file){
-      console.log(file);
-      const storage = getStorage();
-      const filePath = "books/" + bookUID.key ;
-      console.log(bookUID.key);
-      const storageRef = sref(storage, filePath);
-      const uploadTask = uploadBytesResumable(storageRef,file);
-      uploadTask.on(
+
+  const bookRef = ref(db, "content/college/Books");
+  const bookUID = push(bookRef);
+
+  //
+  function uploadHandler(file) {
+    console.log(file);
+    const storage = getStorage();
+    const filePath = "books/" + bookUID.key;
+    console.log(bookUID.key);
+    const storageRef = sref(storage, filePath);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
       "state_changed",
       (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
 
-          setProgress(progress);
+        setProgress(progress);
       },
       (error) => {
-          console.log("error in uploading");
+        console.log("error in uploading");
       },
       () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           console.log("File available at", downloadURL);
           setBooks();
-          function setBooks (){
-             
-            set(bookUID,{
+          function setBooks() {
+            set(bookUID, {
               pdfURL: downloadURL,
-              bookName:BookName.current.value,
-              Author:author.current.value,
-              Subject:subject.current.value,
-              semester:sem.current.value
-      
-            }).then(()=>{
+              bookName: BookName.current.value,
+              Author: author.current.value,
+              Subject: subject.current.value,
+              semester: sem.current.value,
+            }).then(() => {
               console.log("sucessful");
-            })
-          };
-          });
+            });
+          }
+        });
       }
-      );
-    }
+    );
+  }
 
-     //
-    
-
+  //
 
   const fetchBooks = () => {
     setLoading(true);
@@ -80,10 +79,10 @@ export default function Books() {
         if (snapshot.exists()) {
           console.log(snapshot.val());
           let books = [];
-          
-           for (const key  in snapshot.val()){
-            books.push((snapshot.val()[key]));
-          };
+
+          for (const key in snapshot.val()) {
+            books.push(snapshot.val()[key]);
+          }
           setToDisplayData(books);
         } else {
           console.log("No data available");
@@ -98,54 +97,76 @@ export default function Books() {
   useEffect(() => {
     fetchBooks();
   }, []);
-    
+
+  function on() {
+    console.log("on");
+    document.getElementById("overlay").style.display = "block";
+  }
+
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
   return (
     <>
       {loading && <p>Loading ... </p>}
-       
-      
-      <button type="button" class="btn btn-outline-success" style={{display:!isAdmin?'none':null}} onClick={()=>{setShowform(!showform)}}>
-           
-        Add Book Details
-        </button>
-      
 
-
-        {showform && 
-        <form
-        action=""
-        style={{
-          width: "18%",
-          borderRadius: "5px",
-          padding: 5,
-          textAlign: "center",
-          border: "1px solid gray",
-          borderTop: "3px solid Seagreen",
-          backgroundColor: "mintcream",
-          //   borderStyle: "ridge",
-        }}
+      <button
+        type="button"
+        class="btn btn-outline-success"
+        style={{ display: !isAdmin ? "none" : null }}
+        onClick={on}
       >
-        <fieldset>
-          <legend>Book Details</legend>
-          <label htmlfor="lname">Book Name</label>
-          <br />
-          <input type="text" id="lname" ref={BookName} name="lname"   />
-          <label htmlfor="fname">Enter subject </label>
-          <br />
-          <input type="text" id="fname" ref={subject} name="fname" />
-          <br />
+        Add Book 
+      </button>
 
-          <label htmlfor="lname">Book author</label>
-          <br />
-          <input type="text" id="lname" ref={author} name="lname" />
-          <br />
-          <label htmlfor="lname">Semester</label>
-          <input type="text" id="lname" ref={sem} name="lname" />
+      <div id="overlay" className={styles.overlay}>
+        <div id="text" className={styles.text}>
+          <form
+            action=""
+            style={
+              {
+                // width: "18%",
+                // borderRadius: "5px",
+                // padding: 5,
+                // textAlign: "center",
+                // border: "1px solid gray",
+                // borderTop: "3px solid Seagreen",
+                // backgroundColor: "mintcream",
+                //   borderStyle: "ridge",
+              }
+            }
+          >
+            <fieldset>
+              <legend>Book Details</legend>
+              <label htmlfor="lname">Book Name</label>
+              <br />
+              <input type="text" id="lname" ref={BookName} name="lname" />
+              <label htmlfor="fname">Enter subject </label>
+              <br />
+              <input type="text" id="fname" ref={subject} name="fname" />
+              <br />
 
-          <br />
-          <AddButton  onClick={uploadHandler} />
-         </fieldset>
-      </form>}
+              <label htmlfor="lname">Book author</label>
+              <br />
+              <input type="text" id="lname" ref={author} name="lname" />
+              <br />
+              <label htmlfor="lname">Semester</label>
+              <input type="text" id="lname" ref={sem} name="lname" />
+
+              <br />
+              <AddButton onClick={uploadHandler} />
+              <button
+                type="button"
+                className="btn btn-secondary "
+                onClick={off}
+              >
+                Cancel
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
 
       <ul>
         {!loading &&
@@ -170,4 +191,81 @@ export default function Books() {
       </ul>
     </>
   );
+  // return (
+  //   <div>
+  //     <div id="overlay" className={styles.overlay}>
+  //       <div id="text" className={styles.text}>
+  //         <form
+  //           action=""
+  //           // style={{
+  //           //   width: "18%",
+  //           //   borderRadius: "5px",
+  //           //   padding: 5,
+  //           //   textAlign: "center",
+  //           //   border: "1px solid gray",
+  //           //   borderTop: "3px solid Seagreen",
+  //           //   backgroundColor: "mintcream",
+  //           //   //   borderStyle: "ridge",
+  //           // }}
+  //         >
+  //           <fieldset>
+  //             <legend>Notes Details</legend>
+  //             <label htmlfor="fname">Enter subject </label>
+  //             <br />
+  //             <input type="text" id="fname" ref={subject} name="fname" />
+  //             <br />
+  //             <br />
+  //             <label htmlfor="lname">Semester</label>
+  //             <input type="text" id="lname" ref={sem} name="lname" />
+  //             <br />
+  //             <AddButton onClick={uploadHandler} />
+  //             <button
+  //               type="button"
+  //               className="btn btn-secondary "
+  //               onClick={off}
+  //             >
+  //               Cancel
+  //             </button>
+  //           </fieldset>
+  //         </form>
+  //       </div>
+  //     </div>
+  //     {/* <button type="button" class="btn btn-outline-success" style={{display:!isAdmin?'none':null}} onClick={()=>{setShowform(!showform)}}>
+
+  //          Add Notes
+  //          </button>
+  //          {showform &&
+  //       <form
+  //       action=""
+  //       style={{
+  //         width: "18%",
+  //         borderRadius: "5px",
+  //         padding: 5,
+  //         textAlign: "center",
+  //         border: "1px solid gray",
+  //         borderTop: "3px solid Seagreen",
+  //         backgroundColor: "mintcream",
+  //         //   borderStyle: "ridge",
+  //       }}
+  //     >
+  //       <fieldset>
+  //         <legend>Notes Details</legend>
+  //         <label htmlfor="fname">Enter subject </label>
+  //         <br />
+  //         <input type="text" id="fname" ref={subject} name="fname" />
+  //         <br />
+  //         <br />
+  //         <label htmlfor="lname">Semester</label>
+  //         <input type="text" id="lname" ref={sem} name="lname" />
+  //         <br />
+  //         <AddButton  onClick={uploadHandler} />
+  //        </fieldset>
+  //     </form>} */}
+  //     <div className="col-4" style={{ border: "2px solid red" }}>
+  //       <button type="button" class="btn btn-success" onClick={on}>
+  //         AddNote
+  //       </button>
+  //     </div>
+  //   </div>
+  // );
 }
